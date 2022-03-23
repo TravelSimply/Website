bc839669efd83e0a45afc221191f8871074b7bef
Manuel Roglan <manueljoseph113@gmail.com>
2022-03-23 12:15:13 -0400

    installed and configured mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
+import {CacheProvider} from '@emotion/react'
+import {ThemeProvider, CssBaseline} from '@mui/material'
+import theme from '../styles/theme'
 import '../styles/globals.css'
+import createCache from '@emotion/cache'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = () => {
+  return createCache({key: 'css'})
+}
+
+function MyApp({ Component, pageProps, emotionCache }) {
+
+  const cache = emotionCache || clientSideEmotionCache()
+
+  return (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
