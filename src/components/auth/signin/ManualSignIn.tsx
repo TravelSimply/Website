a78963e9a183568893d5f484522e16c131b2c19b
Manuel Roglan <manueljoseph113@gmail.com>
2022-03-26 15:50:05 -0400

    added manual sign in

--- src/components/auth/signin/ManualSignIn.tsx
+++ src/components/auth/signin/ManualSignIn.tsx
@@ -2,12 +2,30 @@ import React from 'react'
 import SignInForm from '../../forms/SignIn'
 import {Props as SignInProps} from '../../forms/SignIn'
 import {FormikHelpers} from 'formik'
+import axios, {AxiosError} from 'axios'
+import Router, {useRouter} from 'next/router'
 
 export default function ManualSignIn() {
 
+    const {query: {redirect}} = useRouter()
+
     const onSubmit = async (vals:SignInProps['vals'], actions:FormikHelpers<SignInProps['vals']>) => {
-        console.log(vals)
-        console.log(actions)
+        try {
+            await axios({
+                method: 'POST',
+                url: '/api/auth/signin',
+                data: vals
+            })
+
+            Router.push({
+                pathname: redirect.toString() || '/dashboard'
+            }) 
+        } catch (e) {
+            if ((e as AxiosError).response.status === 409) {
+                actions.setFieldError(e.response.data.field, e.response.data.msg)
+            }
+            actions.setSubmitting(false)
+        }
     }
 
     return (
---
+++ src/pages/api/auth/signin.ts
@@ -0,0 +1,49 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getUserFromEmail } from "../../../utils/users";
+import { isVerificationTokenWithEmail } from "../../../utils/verificationTokens";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import {setCookie} from 'nookies'
+
+export default async function SignIn(req:NextApiRequest, res:NextApiResponse) {
+
+    if (req.method !== 'POST') {
+        return res.status(400).json({msg: 'Ok...'})
+    }
+
+    try {
+
+        const {email, password} = req.body
+
+        const user = await getUserFromEmail(email)
+
+        if (!user) {
+            const msg = await isVerificationTokenWithEmail(email) ? 'This email is not verified.' : 'This email is not registered.'
+            return res.status(409).json({field: 'email', msg})
+        }
+
+        const isCorrectPassword = await new Promise<boolean>((promiseResult, reject) => {
+            bcrypt.compare(password, user.data.password, (err, result) => {
+                if (err) reject(err)
+                promiseResult(result)
+            }) 
+        })
+
+        if(!isCorrectPassword) {
+            return res.status(409).json({field: 'password', msg: 'Incorrect password.'})
+        }
+
+        const jwtToken = jwt.sign({email}, process.env.TOKEN_SIGNATURE, {expiresIn: '48hr'})
+
+        setCookie({res}, 'auth', jwtToken, {
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: true,
+            maxAge: 172800,
+            path: '/'
+        })
+
+        return res.status(200).json({msg: 'Success'})
+    } catch (e) {
+        return res.status(500).json({msg: 'Internal Server Error'})
+    }
+}
