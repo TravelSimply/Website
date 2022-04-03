dba1451bb35590ca15a65100793f3d432f4bee2d
Manuel Roglan <manueljoseph113@gmail.com>
2022-04-03 04:12:15 -0400

    added swr for friends

--- src/components/account/profile/friends/index/Main.tsx
+++ src/components/account/profile/friends/index/Main.tsx
@@ -1,29 +1,46 @@
-// import { Box, Container } from '@mui/material';
-// import React, { useState } from 'react'
-// import { ClientUser } from '../../../../../database/interfaces'
-// import {PrimarySearchBar} from '../../../../misc/searchBars'
-// // import useSWR from 'swr'
+import { Box, CircularProgress, Container, Typography } from '@mui/material';
+import React, { useState } from 'react'
+import { ClientUser } from '../../../../../database/interfaces'
+import {PrimarySearchBar} from '../../../../misc/searchBars'
+import useSWR from 'swr'
 
-// interface Props {
-//     user: ClientUser;
-// }
+interface Props {
+    user: ClientUser;
+}
 
-// export default function Main({user}:Props) {
+export default function Main({user}:Props) {
 
-//     const [search, setSearch] = useState('')
+    const [search, setSearch] = useState('')
 
-//     // const {data:friends, error} = useSWR('/api/users/profile/friends', {dedupingInterval: 3600000, revalidateOnFocus: false, 
-//     //     revalidateOnReconnect: false})
+    const [searchFriends, setSearchFriends] = useState([])
 
-//     // console.log(friends)
+    const {data:friends, error} = useSWR('/api/users/profile/friends/all', {revalidateOnFocus: false, 
+        revalidateOnReconnect: false})
 
-//     return (
-//         <Box maxWidth="md" m={3}>
-//             <Box>
-//                 <Container maxWidth="sm">
-//                     <PrimarySearchBar search={search} setSearch={setSearch} />
-//                 </Container>
-//             </Box>
-//         </Box>
-//     )
-// }
+    console.log(friends)
+
+    return (
+        <Box maxWidth="md" m={3}>
+            <Box>
+                <Container maxWidth="sm">
+                    <PrimarySearchBar search={search} setSearch={setSearch} />
+                </Container>
+            </Box>
+            {error ? <Box textAlign="center" mt={3}>
+                Error loading friends, retrying... 
+            </Box> : !friends && searchFriends.length === 0 ? <Box mt={3}>
+                <Box mx="auto" textAlign="center">
+                    <CircularProgress />
+                </Box> 
+            </Box> : friends.length === 0 ? <Box mt={3}>
+                <Box textAlign="center" mx="auto" maxWidth="sm">
+                    <Typography variant="h6">
+                        Search for friends to add!
+                    </Typography>
+                </Box>
+            </Box> : <Box>
+                here are the friends 
+            </Box>}
+        </Box>
+    )
+}
