a98254e87959926f2f7d131164430f839d002874
Manuel Roglan <manueljoseph113@gmail.com>
2022-04-03 22:12:19 -0400

    updated FriendCard ui

--- src/components/account/profile/friends/index/FriendCard.tsx
+++ src/components/account/profile/friends/index/FriendCard.tsx
@@ -1,7 +1,7 @@
-import { Avatar, Box, Grid, Paper, Typography } from '@mui/material';
+import { Avatar, Box, Grid, ListItemText, Paper, Typography } from '@mui/material';
 import React, { useMemo, useState } from 'react'
 import { ClientFilteredUser, ClientUser } from '../../../../../database/interfaces'
-import { OrangeDenseSecondaryButton } from '../../../../mui-customizations/buttons';
+import { OrangeDensePrimaryButton, OrangeDenseSecondaryButton } from '../../../../mui-customizations/buttons';
 import {mutate} from 'swr'
 
 interface Props {
@@ -33,38 +33,30 @@ export default function FriendCard({users, friendIndex}:Props) {
 
     return (
         <Paper>
-            <Box mt={3}>
+            <Box mt={3} mx={3}>
                 <Grid container direction="column" justifyContent="space-between">
                     <Grid item>
-                        <Grid container alignItems="center">
+                        <Grid container justifyContent="center" >
                             <Grid item>
                                 <Box m={1}>
-                                    <Avatar sx={{width: 100, height: 100}}
+                                    <Avatar sx={{width: {xs: 50, sm: 100}, height: {xs: 50, sm: 100}}}
                                     src={friend.data.image?.src || '/default_profile.png'} imgProps={{referrerPolicy: 'no-referrer'}} />
                                 </Box>
                             </Grid>
-                            <Grid item>
+                            <Grid item sx={{paddingY: 1}}>
                                 <Box mx={1}>
-                                    <Box mb={2}>
-                                        <Typography variant="body1">
-                                            @{friend.data.username}
-                                        </Typography>
-                                    </Box>
-                                    <Box>
-                                        <Typography variant="subtitle1">
-                                            {friend.data.firstName} {friend.data.lastName}
-                                        </Typography>
-                                    </Box>
+                                    <ListItemText primary={`@${friend.data.username}`} 
+                                    secondary={friend.data.firstName + ' ' + friend.data.lastName} />
                                 </Box>
                             </Grid>
                         </Grid>
                     </Grid>
                     <Grid item>
-                        <Box mt={0} p={1} bgcolor="orangeBg.light">
+                        <Box mx={-3} pl={3} py={1} bgcolor="orangeBg.light">
                             <Box>
-                                <OrangeDenseSecondaryButton disabled={loading} onClick={() => removeFriend()}>
+                                <OrangeDensePrimaryButton disabled={loading} onClick={() => removeFriend()}>
                                     Remove Friend
-                                </OrangeDenseSecondaryButton> 
+                                </OrangeDensePrimaryButton> 
                             </Box>
                         </Box>
                     </Grid>
