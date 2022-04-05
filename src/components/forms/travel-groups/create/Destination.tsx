7d445d3dae91e369f2a5eec98bac5645a962b326
Manuel Roglan <manueljoseph113@gmail.com>
2022-04-05 19:20:29 -0400

    added Destination form

---
+++ src/components/forms/travel-groups/create/Destination.tsx
@@ -0,0 +1,89 @@
+import React, { Dispatch, SetStateAction, useMemo, useState } from 'react'
+import {Box, FormGroup} from '@mui/material'
+import {Form, Formik, FormikHelpers} from 'formik'
+import {FormikSelectField, FormikTextField} from '../../FormikFields'
+import {object, string} from 'yup'
+import FormObserver from '../../FormObserver'
+import { TravelGroup } from '../../../../database/interfaces'
+import {MenuItem, Autocomplete, TextField} from '@mui/material'
+import { getCountries, getStates } from 'country-state-picker'
+
+export interface Props {
+    vals: TravelGroup['data']['destination'];
+    onSubmit: (vals:Props['vals'], actions:FormikHelpers<Props['vals']>) => void;
+    setFormContext?: Dispatch<SetStateAction<any>>;
+}
+
+const possibleRegions = ['Interregional', 'U.S. & Canada', 'Central America', 'South America', 'Europe', 
+    'Asia', 'Africa', 'Oceania', 'Antarctica']
+
+export default function Destination({vals, onSubmit, setFormContext}:Props) {
+
+    const initialVals = vals
+
+    const countries = useMemo(() => getCountries(), [])
+
+    const [countryCode, setCountryCode] = useState('')
+
+    const states = useMemo(() => getStates(countryCode), [countryCode])
+
+    return (
+        <Box>
+            <Formik validationSchema={object({
+                    region: string().oneOf(possibleRegions),
+                    country: string(),
+                    state: string().max(50),
+                    city: string().max(50),
+                    address: string().max(100)
+                })}
+                initialValues={initialVals} onSubmit={(values, actions) => onSubmit(values, actions)}>
+                {({values, errors, isSubmitting, isValidating, setFieldValue, touched}) => (
+                    <Form>
+                        {setFormContext && <FormObserver setFormContext={setFormContext} />}
+                        <Box my={3}>
+                            <FormGroup>
+                                <FormikSelectField name="region" label="Region" value={values.region}>
+                                    {possibleRegions.map(region => (
+                                        <MenuItem key={region} value={region}>
+                                            {region}
+                                        </MenuItem>
+                                    ))}
+                                </FormikSelectField>
+                            </FormGroup>
+                        </Box>
+                        {values.region !== 'Interregional' && <Box my={3}>
+                            <FormGroup>
+                                <Autocomplete renderInput={(params) => (
+                                    <TextField {...params} label="Country" error={touched.country && Boolean(errors.country)}
+                                    helperText={touched.country && errors.country ? errors.country : ''} />
+                                )} options={countries.map(country => country)} getOptionLabel={(option:any) => option.name || option} 
+                                onChange={(e, value) => {
+                                    setFieldValue('country', value?.name || '')
+                                    setCountryCode(value?.code || '')
+                                }} />
+                            </FormGroup>
+                        </Box>}
+                        {values.country && <Box my={3}>
+                            <FormGroup>
+                                <Autocomplete renderInput={(params) => (
+                                    <TextField {...params} label="State/Province" error={touched.state && Boolean(errors.state)}
+                                    helperText={touched.state && errors.state ? errors.state : ''} />
+                                )} options={states} onChange={(e, value) => setFieldValue('state', value || '')} />
+                            </FormGroup>
+                       </Box>}
+                        {values.state && <Box my={3}>
+                            <FormGroup>
+                                <FormikTextField name="city" label="City" />
+                            </FormGroup>
+                        </Box>}
+                        {values.city && <Box my={3}>
+                            <FormGroup>
+                                <FormikTextField name="address" label="Address" />
+                            </FormGroup> 
+                        </Box>}
+                    </Form>
+                )}
+            </Formik>
+        </Box>
+    )
+}
