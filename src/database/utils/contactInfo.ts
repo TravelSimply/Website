37fdfe184db1f46a3d5922e8cbc7a1324c29823c
Manuel Roglan <manueljoseph113@gmail.com>
2022-04-20 17:54:30 -0400

    added getUserContactInfo

---
+++ src/database/utils/contactInfo.ts
@@ -0,0 +1,19 @@
+import {query as q} from 'faunadb'
+import client from '../fauna'
+import { ContactInfo } from '../interfaces'
+
+export function getUserContactInfoInnerQuery(userId:string) {
+    
+    return q.If(
+        q.Exists(q.Match(q.Index('contactInfo_by_userId'), userId)),
+        q.Get(q.Match(q.Index('contactInfo_by_userId'), userId)),
+        q.Create(q.Collection('contactInfo'), {data: {userId}})
+    )
+}
+
+export async function getUserContactInfo(userId:string):Promise<ContactInfo> {
+
+    return await client.query(
+        getUserContactInfoInnerQuery(userId)
+    )
+}
