b22b2f24d92318865a1e9f89f85e32891adbb277
Manuel Roglan <manueljoseph113@gmail.com>
2022-04-23 23:15:35 -0400

    added sections for travellers, invites, requests

--- src/components/travel-groups/[id]/travellers/Main.tsx
+++ src/components/travel-groups/[id]/travellers/Main.tsx
@@ -1,5 +1,10 @@
-import { Box, Container, Divider, Grid, Paper, Typography } from "@mui/material";
-import { ClientTravelGroup, ClientTravelGroupWithPopulatedTravellersAndContactInfo, ClientUser } from "../../../../database/interfaces";
+import { Box, ButtonGroup, Container, Divider, Grid, Paper, Typography } from "@mui/material";
+import { useMemo, useState } from "react";
+import { ClientTravelGroup, ClientTravelGroupWithPopulatedTravellersAndContactInfo, ClientUser, ClientUserWithContactInfo } from "../../../../database/interfaces";
+import { searchForUsers } from "../../../../utils/search";
+import { darkPrimaryOnHover } from "../../../misc/animations";
+import { PrimarySearchBar } from "../../../misc/searchBars";
+import { OrangeButtonGroup } from "../../../mui-customizations/buttonGroup";
 import TravellerCard from "./TravellerCard";
 
 interface Props {
@@ -9,7 +14,22 @@ interface Props {
 
 export default function Main({user, travelGroup}:Props) {
 
-    console.log(travelGroup)
+    const [mode, setMode] = useState('travellers')
+    const modes = ['travellers', 'invites', 'requests']
+
+    const [travellers, setTravellers] = useState(travelGroup.data.members)
+    const [searchedTravellers, setSearchedTravellers] = useState(travellers)
+
+    const [search, setSearch] = useState('')
+
+    useMemo(() => {
+
+        if (!search.trim()) {
+            setSearchedTravellers(travellers)
+        }
+
+        setSearchedTravellers(searchForUsers(search, travellers) as ClientUserWithContactInfo[])
+    }, [search])
 
     return (
         <Box>
@@ -60,17 +80,39 @@ export default function Main({user, travelGroup}:Props) {
                                     <Divider />
                                 </Box>
                             </Box>
+                            <Box textAlign="center" my={2}>
+                                <OrangeButtonGroup selected={mode} setSelected={setMode} 
+                                options={[
+                                    {value: 'travellers', text: `${travellers.length} Travellers`},
+                                    {value: 'invites', text: '0 Invites'},
+                                    {value: 'requests', text: '0 Join Requests'}
+                                ]} />
+                            </Box>
+                            <Box mb={2}>
+                                <Box mx="auto" maxWidth="sm">
+                                    <PrimarySearchBar search={search} setSearch={setSearch} />
+                                </Box>
+                            </Box>
                             <Box>
+                                {mode === 'travellers' ? 
                                 <Box>
-                                    <Grid container alignItems="stretch" justifyContent="space-around" spacing={3}>
-                                        {travelGroup.data.members.map((member, i) => (
-                                            <Grid item key={i} flexBasis={600}>
+                                    <Grid container alignItems="stretch" justifyContent="space-around">
+                                        {searchedTravellers.map((member, i) => (
+                                            <Grid item key={i} flexBasis={600} sx={{mb: 3, mx: 1}}>
                                                 <TravellerCard user={user} isAdmin={travelGroup.data.owner === user.ref['@ref'].id}
                                                 traveller={member} />
                                             </Grid>
                                         ))}
                                     </Grid>
+                                </Box> :
+                                mode === 'invites' ?
+                                <Box>
+                                    Invites
+                                </Box> :
+                                <Box>
+                                    Requests
                                 </Box>
+                                }
                             </Box>
                         </Box>
                     </Grid>
