5b7563e133451c3b93667d10a21f25a4ec6d7b13
Manuel Roglan <manueljoseph113@gmail.com>
2022-05-10 14:35:26 -0400

    created Overview component

--- src/components/travel-groups/[id]/cards/ProposalPreview.tsx
+++ src/components/travel-groups/[id]/cards/ProposalPreview.tsx
@@ -4,6 +4,7 @@ import { Container, Dialog, Box, Paper, Grid, Avatar, Typography, AppBar, IconBu
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import CloseIcon from '@mui/icons-material/Close'
 import {getDestination as getGeneralDestination} from '../../index/TravelGroupCard'
+import Overview from "../index/Overview";
 
 interface Props {
     open: boolean;
@@ -22,10 +23,11 @@ export default function ProposalPreview({open, close, travelGroup:dbTravelGroup,
                 name: proposal.data.data.name || dbTravelGroup.data.name,
                 desc: proposal.data.data.desc || dbTravelGroup.data.desc,
                 destination: proposal.data.data.destination || dbTravelGroup.data.destination,
-                date: proposal.data.data.date || dbTravelGroup.data.date,
+                date: {...(proposal.data.data.date || dbTravelGroup.data.date), roughly: false, unknown: false,
+                    estLength: [0, 'days']},
                 image: proposal.data.data.image || dbTravelGroup.data.image
             }
-        }
+        } as ClientTravelGroup
     }, [])
 
     return (
@@ -41,47 +43,7 @@ export default function ProposalPreview({open, close, travelGroup:dbTravelGroup,
                 <Box mb={3}>
                     <Paper>
                         <Box p={3}>
-                            <Box>
-                                <Grid container spacing={3}>
-                                    <Grid item>
-                                        <Avatar sx={{width: {md: 200, sm: 150, xs: 150}, height: {md: 200, sm: 150, xs: 150},
-                                        borderRadius: 2}}
-                                        src={travelGroup.data.image?.src || '/default_travelgroup.png'}
-                                        variant="square" />
-                                    </Grid>
-                                    <Grid item>
-                                        <Box>
-                                            <Box mb={2}>
-                                                <Typography variant="h4">
-                                                    {travelGroup.data.name}     
-                                                </Typography>     
-                                            </Box>
-                                            <Box ml={2} mb={2}>
-                                                <Grid container wrap="nowrap" alignItems="start">
-                                                    <Grid item>
-                                                        <LocationOnIcon sx={{mt: 0.5, color: 'secondary.main'}} />
-                                                    </Grid>
-                                                    <Grid item>
-                                                        <Box ml={1}>
-                                                            {travelGroup.data.destination.address && <Typography variant="h6">
-                                                                {travelGroup.data.destination.address},
-                                                            </Typography>}
-                                                            <Typography variant="h6">
-                                                                {getGeneralDestination(travelGroup.data.destination)}
-                                                            </Typography>
-                                                        </Box>
-                                                    </Grid>
-                                                </Grid>
-                                            </Box>
-                                            <Box ml={2}>
-                                                <Typography variant="body1">
-                                                    {travelGroup.data.desc}
-                                                </Typography>
-                                            </Box>
-                                        </Box> 
-                                    </Grid>
-                                </Grid>
-                            </Box>
+                            <Overview travelGroup={travelGroup} />
                         </Box>
                     </Paper>
                 </Box>
--- src/components/travel-groups/[id]/index/Main.tsx
+++ src/components/travel-groups/[id]/index/Main.tsx
@@ -12,6 +12,7 @@ import EditOverview from './EditOverview'
 import ProposeDate from "./ProposeDate";
 import ModifyDate from "./ModifyDate";
 import Snackbar from '../../../misc/snackbars'
+import Overview from "./Overview";
 
 interface Props {
     user: ClientUser;
@@ -81,47 +82,7 @@ export default function Main({user, travelGroup:dbTravelGroup}:Props) {
                             isAdmin={travelGroup.data.owner === user.ref['@ref'].id}
                             junkIds={user.data.junkImagePublicIds}
                             onEditComplete={onEditComplete} /> 
-                            : <Box>
-                                <Grid container spacing={3}>
-                                    <Grid item>
-                                        <Avatar sx={{width: {md: 200, sm: 150, xs: 150}, height: {md: 200, sm: 150, xs: 150},
-                                        borderRadius: 2}}
-                                        src={travelGroup.data.image?.src || '/default_travelgroup.png'}
-                                        variant="square" />
-                                    </Grid>
-                                    <Grid item>
-                                        <Box>
-                                            <Box mb={2}>
-                                                <Typography variant="h4">
-                                                    {travelGroup.data.name}     
-                                                </Typography>     
-                                            </Box>
-                                            <Box ml={2} mb={2}>
-                                                <Grid container wrap="nowrap" alignItems="start">
-                                                    <Grid item>
-                                                        <LocationOnIcon sx={{mt: 0.5, color: 'secondary.main'}} />
-                                                    </Grid>
-                                                    <Grid item>
-                                                        <Box ml={1}>
-                                                            {travelGroup.data.destination.address && <Typography variant="h6">
-                                                                {travelGroup.data.destination.address},
-                                                            </Typography>}
-                                                            <Typography variant="h6">
-                                                                {getGeneralDestination(travelGroup.data.destination)}
-                                                            </Typography>
-                                                        </Box>
-                                                    </Grid>
-                                                </Grid>
-                                            </Box>
-                                            <Box ml={2}>
-                                                <Typography variant="body1">
-                                                    {travelGroup.data.desc}
-                                                </Typography>
-                                            </Box>
-                                        </Box> 
-                                    </Grid>
-                                </Grid>
-                            </Box>}
+                            : <Overview travelGroup={travelGroup} />}
                             <Box position="absolute" top="0px" right="0px">
                                 <OrangePrimaryIconButton onClick={() => setEditing(!editing)}>
                                     {editing ? <CancelIcon sx={{fontSize: 30}} /> :
