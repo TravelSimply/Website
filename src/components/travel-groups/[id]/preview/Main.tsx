cc24beea30a9ccf71e245313a38c37dbe3ee014e
Manuel Roglan <manueljoseph113@gmail.com>
2022-05-10 15:57:08 -0400

    showing date

--- src/components/travel-groups/[id]/preview/Main.tsx
+++ src/components/travel-groups/[id]/preview/Main.tsx
@@ -1,5 +1,8 @@
-import { Box, Container, Paper } from "@mui/material";
+import { Box, Container, Paper, Typography } from "@mui/material";
+import dayjs from "dayjs";
+import { useMemo } from "react";
 import { ClientTravelGroup, ClientUser } from "../../../../database/interfaces";
+import Calendar from "../../../calendar/Calendar";
 import Overview from "../index/Overview";
 
 interface Props {
@@ -9,13 +12,47 @@ interface Props {
 
 export default function Main({user, travelGroup}:Props) {
 
+    const [start, end] = useMemo(() => {
+        return [
+            dayjs(travelGroup.data.date.start).format('MMMM D'),
+            dayjs(travelGroup.data.date.end).format('MMMM D')
+        ]
+    }, [])
+
+    const estLength = useMemo(() => {
+        return `${travelGroup.data.date.estLength[0]} 
+        ${travelGroup.data.date.estLength[1].substring(0, travelGroup.data.date.estLength[1].length - 1)}`
+    }, [])
+
     return (
         <Box mt={3} mx={3}>
             <Container maxWidth="md">
-                <Box>
+                <Box mb={3}>
                     <Paper>
                         <Box p={3}>
                             <Overview travelGroup={travelGroup} />
+                        </Box>
+                    </Paper>
+                </Box>
+                <Box>
+                    <Paper>
+                        <Box p={3}>
+                            <Box textAlign="center" mb={2}>
+                                <Typography variant="h6">
+                                    {travelGroup.data.date.unknown ? 
+                                    `${estLength} trip.` : 
+                                    travelGroup.data.date.roughly ?
+                                    `${estLength} trip sometime between ${start} and ${end}.` :
+                                    `Traveling from ${start} to ${end}.`
+                                    }
+                                </Typography>
+                                <Box>
+                                    {!travelGroup.data.date.unknown && <Calendar 
+                                    dateRange={[dayjs(travelGroup.data.date.start), dayjs(travelGroup.data.date.end)]}
+                                    onDateRangeChange={() => {}} availability={{ref: {'@ref': {id: '1', ref: null}}, data: {userId: '1', dates: {}}}}
+                                    displayOnly startDate={dayjs(travelGroup.data.date.start)} />}
+                                </Box>
+                            </Box>
                         </Box>
                     </Paper>
                 </Box>
