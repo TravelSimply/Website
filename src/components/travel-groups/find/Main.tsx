a8276b6b3e8c8379a48e1733b4fc2e64b5bcb49c
Manuel Roglan <manueljoseph113@gmail.com>
2022-05-12 15:24:00 -0400

    added messages for none found or no friends

--- src/components/travel-groups/find/Main.tsx
+++ src/components/travel-groups/find/Main.tsx
@@ -1,12 +1,13 @@
-import { Box, CircularProgress, Container } from "@mui/material";
+import { Box, CircularProgress, Container, Typography } from "@mui/material";
 import useSWR from "swr";
-import { ClientBareTravelGroupInfo, ClientUser } from "../../../database/interfaces";
+import { ClientBareTravelGroupInfo, ClientTravelGroup, ClientUser } from "../../../database/interfaces";
 import {useRouter} from 'next/router'
 import { useSearchedTravelGroups } from "../../hooks/travelGroups";
 import Search from "./Search";
 import { useMemo, useState } from "react";
 import TravelGroupCard from "../index/TravelGroupCard";
 import { OrangePrimaryButton } from "../../mui-customizations/buttons";
+import Link from 'next/link'
 
 interface Props {
     user: ClientUser;
@@ -32,12 +33,35 @@ export default function Main({user}:Props) {
                     <Box textAlign="center">
                         <CircularProgress />
                     </Box>
-                </Box>: 
-                <Box>
+                </Box> : 
+                !user.data.friends || user.data.friends?.length === 0 ? <Box>
+                    <Container maxWidth="sm">
+                        <Box textAlign="center">
+                            <Typography variant="h6" gutterBottom>
+                                Add your friends to find groups to join! 
+                            </Typography>
+                            <Link href="/profile/friends/add">
+                                <a>
+                                    <OrangePrimaryButton>
+                                        Add Friends
+                                    </OrangePrimaryButton>
+                                </a>
+                            </Link>
+                        </Box>
+                    </Container>
+                </Box> : <Box>
                     {filteredGroups === 0 ? <Box textAlign="center">
                         An error occured while loading Travel Groups.
-                    </Box> : !filteredGroups ? '' : <Box>
-                        {filteredGroups.map(group => (
+                    </Box> : !filteredGroups ? '' : (filteredGroups as ClientTravelGroup[]).length === 0 ? <Box>
+                        <Container maxWidth="sm">
+                            <Box textAlign="center">
+                                <Typography variant="h6">
+                                    No groups found! Try widening your search.
+                                </Typography>
+                            </Box>
+                        </Container>
+                    </Box> : <Box>
+                        {(filteredGroups as ClientTravelGroup[]).map(group => (
                             <Box key={group.ref['@ref'].id}>
                                 <TravelGroupCard travelGroup={group} user={user} />
                             </Box>
