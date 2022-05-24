0f8e2831a65f80f52aa3ed3d71d22ba1cc704442
Manuel Roglan <manueljoseph113@gmail.com>
2022-05-24 18:24:27 -0400

    fixed Travel Group preview

--- src/components/travel-groups/[id]/preview/Main.tsx
+++ src/components/travel-groups/[id]/preview/Main.tsx
@@ -15,18 +15,6 @@ interface Props {
 
 export default function Main({user, travelGroup, invites, joinRequests}:Props) {
 
-    const [start, end] = useMemo(() => {
-        return [
-            dayjs(travelGroup.data.date.start).format('MMMM D'),
-            dayjs(travelGroup.data.date.end).format('MMMM D')
-        ]
-    }, [])
-
-    const estLength = useMemo(() => {
-        return `${travelGroup.data.date.estLength[0]} 
-        ${travelGroup.data.date.estLength[1].substring(0, travelGroup.data.date.estLength[1].length - 1)}`
-    }, [])
-
     return (
         <Box mt={3} mx={3}>
             <Container maxWidth="md">
@@ -38,28 +26,6 @@ export default function Main({user, travelGroup, invites, joinRequests}:Props) {
                     <Paper>
                         <Box p={3}>
                             <Overview travelGroup={travelGroup} />
-                        </Box>
-                    </Paper>
-                </Box>
-                <Box>
-                    <Paper>
-                        <Box p={3}>
-                            <Box textAlign="center" mb={2}>
-                                <Typography variant="h6">
-                                    {travelGroup.data.date.unknown ? 
-                                    `${estLength} trip.` : 
-                                    travelGroup.data.date.roughly ?
-                                    `${estLength} trip sometime between ${start} and ${end}.` :
-                                    `Traveling from ${start} to ${end}.`
-                                    }
-                                </Typography>
-                                <Box>
-                                    {!travelGroup.data.date.unknown && <Calendar 
-                                    dateRange={[dayjs(travelGroup.data.date.start), dayjs(travelGroup.data.date.end)]}
-                                    onDateRangeChange={() => {}} availability={{ref: {'@ref': {id: '1', ref: null}}, data: {userId: '1', dates: {}}}}
-                                    displayOnly startDate={dayjs(travelGroup.data.date.start)} />}
-                                </Box>
-                            </Box>
                         </Box>
                     </Paper>
                 </Box>
