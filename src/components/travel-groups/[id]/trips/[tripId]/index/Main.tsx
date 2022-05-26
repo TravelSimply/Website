84b384dce2dc01d34fb67b58dabff503b784c6a6
Manuel Roglan <manueljoseph113@gmail.com>
2022-05-26 16:47:59 -0400

    added date viewer to overview

--- src/components/travel-groups/[id]/trips/[tripId]/index/Main.tsx
+++ src/components/travel-groups/[id]/trips/[tripId]/index/Main.tsx
@@ -1,7 +1,9 @@
-import { Box, Container, Paper } from "@mui/material";
-import dayjs from "dayjs";
+import { Box, Container, Paper, Typography } from "@mui/material";
+import dayjs, { Dayjs } from "dayjs";
 import { useMemo } from "react";
-import { ClientTripWithTravelGroupBareInfo, ClientUser } from "../../../../../../database/interfaces";
+import { ClientPopulatedAvailability, ClientTripWithTravelGroupBareInfo, ClientUser } from "../../../../../../database/interfaces";
+import Calendar from "../../../../../calendar/Calendar";
+import { OrangePrimaryButton } from "../../../../../mui-customizations/buttons";
 import Overview from "./Overview";
 
 interface Props {
@@ -11,12 +13,24 @@ interface Props {
 
 export default function Main({user, trip}:Props) {
 
-    const [startDate, endDate] = useMemo(() => {
-        return [
-            dayjs(trip.data.date.start['@date']).format('MMMM D'),
-            dayjs(trip.data.date.end['@date']).format('MMMM D')
+    const dateRange:[Dayjs, Dayjs] = useMemo(() => {
+        return trip.data.date.unknown ? [null, null] : [
+            dayjs(trip.data.date.start['@date']),
+            dayjs(trip.data.date.end['@date'])
         ]
     }, [])
+
+    const dummyAvailability:ClientPopulatedAvailability = useMemo(() => {
+        return {
+            ref: {'@ref': {id: '', ref: null}},
+            data: {
+                userId: '',
+                dates: {}
+            }
+        }
+    }, [])
+
+    const onDateRangeChange = () => {}
 
     return (
         <Box my={3}>
@@ -25,6 +39,39 @@ export default function Main({user, trip}:Props) {
                     <Paper>
                         <Box p={3}>
                             <Overview trip={trip} /> 
+                        </Box>
+                    </Paper>
+                </Box>
+                <Box>
+                    <Paper>
+                        <Box p={3}>
+                            {trip.data.date.unknown ? <Box>
+                                <Box textAlign="center" mb={2}>
+                                    <Typography variant="h6">
+                                        No set date for travel yet
+                                    </Typography>
+                                </Box> 
+                                <Box position="relative">
+                                    <Box sx={{opacity: 0.3}}>
+                                        <Calendar dateRange={dateRange} onDateRangeChange={onDateRangeChange}
+                                        availability={dummyAvailability} displayOnly />
+                                    </Box>
+                                    <OrangePrimaryButton sx={{minWidth: 200, position: 'absolute', top: '50%',
+                                    left: '50%', transform: 'translate(-50%, -50%)'}}>
+                                        Propose a Date
+                                    </OrangePrimaryButton>
+                                </Box>
+                            </Box> : <Box>
+                                <Box textAlign="center" mb={2}>
+                                    <Typography variant="h6">
+                                        Traveling from {dateRange[0].format('MMMM D')} to {dateRange[1].format('MMMM D')}
+                                    </Typography>
+                                    <Box>
+                                        <Calendar dateRange={dateRange} onDateRangeChange={onDateRangeChange}
+                                        availability={dummyAvailability} displayOnly />
+                                    </Box>
+                                </Box>
+                            </Box>}
                         </Box>
                     </Paper>
                 </Box>
